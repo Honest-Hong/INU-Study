53b942ad9e8ddc8ece53954722124162bf87675f
Onedelay <wjy0218@gmail.com>
2017-04-10 17:32:05 +0900

    2

---
+++ ONE/for/for_11.py
@@ -0,0 +1,10 @@
+arr = list(str(input()))
+arr.reverse()
+length = len(arr)
+cnt = 1
+
+for i in range(length):
+    print(arr.pop(),end='')
+    if cnt % 10 == 0 : print()
+    cnt = cnt + 1
+print()
