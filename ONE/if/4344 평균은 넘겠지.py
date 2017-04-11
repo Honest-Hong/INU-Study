94a7ee6e0a407c662a0f9ab354761e0dc7f628f8
Onedelay <wjy0218@gmail.com>
2017-04-11 19:50:48 +0900

    백준문제풀이

---
+++ ONE/if/4344 평균은 넘겠지.py
@@ -0,0 +1,13 @@
+N = int(input())
+result = list()
+cnt = 0
+for i in range(N):
+    score = [int(i) for i in input().split()]
+    avg = sum(score[1:])/score[0]
+    for j in score[1:]:
+        if j > avg: cnt += 1                  
+    result.append(round((cnt/score[0])*100,3))
+    cnt = 0
+
+for i in range(len(result)):
+    print("%.3f%%" % result[i])
